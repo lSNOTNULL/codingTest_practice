aaca50c6367816fa9f2ac09b8c8e3d81f1df4d2a
Junho Cho <iream1@naver.com>
2025-10-26 05:28:24 +0900

    [Bronze V] Title: 이상한 기호, Time: 96 ms, Memory: 14148 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+           BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int answer = (A+B)*(A-B);
+            
+            bw.write(String.valueOf(answer));
+            bw.flush();
+        }
+    }
+}            
