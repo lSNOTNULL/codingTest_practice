cb642aea38d2cf78c4f2f3186750b5087df2b226
Junho Cho <iream1@naver.com>
2025-10-31 04:29:45 +0900

    [Bronze V] Title: 제리와 톰, Time: 124 ms, Memory: 15964 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+           BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int answer = (B - A);
+            bw.write(String.valueOf(answer + " " + B));
+            bw.flush();
+        }
+    }
+}
